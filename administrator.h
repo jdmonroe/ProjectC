52fa086c912fdc846416be383d245db5e5af33bb
Jacob Monroe <jacob.monroe@ttu.edu>
2017-04-24 13:35:08 -0500

    Project C
    Project file for C

---
+++ administrator.h
@@ -0,0 +1,11 @@
+#ifndef ADMINISTRATOR_H_INCLUDED
+#define ADMINISTRATOR_H_INCLUDED
+
+void CreateAccount(FILE* file);
+void ChangePassword(char password);
+char ViewInfo(FILE* file);
+void DeleteInfo(FILE* file);
+char ShowTop(FILE* file);
+char ShowAll(FILE* file);
+
+#endif // ADMINISTRATOR_H_INCLUDED
---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+int i, j;
+char dataArray[10][500];
+FILE *dataFile;
+dataFile = fopen("CustomerData.txt", "r");
+
+
+for (i = 0; i < 8;){
+    for (j = 0; j < 10; j++){
+        fscanf(dataFile, "%s", &dataArray[i][j]);
+        if(dataArray[i][j] == "/n"){
+            i++;
+        }
+    }
+}
+fclose(dataFile);
+for(i = 0; i < 10; i++){
+    printf("%s", dataArray[i]);
+}
+}
