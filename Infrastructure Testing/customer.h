6fdbe4b3fb00d6dab6b6ac00168070d508f8b693
Juan Ruiz <manuelruizju@yahoo.com>
2017-04-30 22:09:53 -0500

    final?
    The customer stuff didn't know what to put in the c file

--- Infrastructure Testing/customer.h
+++ Infrastructure Testing/customer.h
@@ -1,6 +1,14 @@
 #ifndef CUSTOMER_H_INCLUDED
 #define CUSTOMER_H_INCLUDED
 
-
-
+void PrintCustomerMenu(void);
+void CustomerMenu(int choice);//Functions for Menu Choices will be inside this function
+                              //  (since you can't call a function inside a function)
+/**Functions inside CustomerMenu:
+ void ChangePassword(int* userID);
+ void ViewCustomer(int* UserID);
+ void ViewBalance(int* userID);
+ void MakeDeposit(int* userID);
+ void TransferMoney(int* userID);
+ void WithdrawMoney (int* userID);**/
 #endif // CUSTOMER_H_INCLUDED
